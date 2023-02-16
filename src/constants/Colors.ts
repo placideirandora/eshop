e8347806f4e7aef25e0e0bf8b6572ab089200fa1
Placide IRANDORA <placideirandora@gmail.com>
2023-02-16 18:43:03 +0200

    chore: create reusable constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,14 @@
+const white = '#fff';
+const black = '#000';
+const dark = '#626262';
+const blue = '#1F41BB';
+const lightBlue = '#f1f4ff';
+
+export default {
+  darkText: dark,
+  text: black,
+  background: white,
+  primary: blue,
+  onPrimary: white,
+  lightPrimary: lightBlue,
+};
