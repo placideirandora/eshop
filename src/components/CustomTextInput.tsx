7490b5fcd7545beb86bb7479471fbf3853a2298a
Placide IRANDORA <placideirandora@gmail.com>
2023-02-16 18:41:26 +0200

    feat: create custom reusable components for text input and floating action button

---
+++ src/components/CustomTextInput.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, TextInputProps} from 'react-native';
+
+import Colors from '../constants/Colors';
+import Spacing from '../constants/Spacing';
+import FontSize from '../constants/FontSize';
+
+const CustomTextInput: React.FC<TextInputProps> = ({...otherProps}) => {
+  const [focusedInput, setFocusedInput] = useState<boolean>(false);
+
+  return (
+    <TextInput
+      onFocus={() => setFocusedInput(true)}
+      onBlur={() => setFocusedInput(false)}
+      placeholderTextColor={Colors.darkText}
+      style={[styles.textInput, focusedInput && styles.textInputFocused]}
+      {...otherProps}
+    />
+  );
+};
+
+export default CustomTextInput;
+
+const styles = StyleSheet.create({
+  textInput: {
+    fontSize: FontSize.small,
+    padding: Spacing * 2,
+    backgroundColor: Colors.lightPrimary,
+    borderRadius: Spacing,
+    width: '90%',
+    marginVertical: Spacing,
+  },
+  textInputFocused: {
+    borderWidth: 2,
+    borderColor: Colors.primary,
+  },
+});
---
+++ src/components/FAB.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
+
+import Colors from '../constants/Colors';
+
+type Props = {
+  navigate: Function;
+  screen: string;
+};
+
+const FAB: React.FC<Props> = ({navigate, screen}) => {
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={() => navigate(screen)}>
+        <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <Path
+            d="M12 6V18"
+            stroke="#fff"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <Path
+            d="M6 12H18"
+            stroke="#fff"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </Svg>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default FAB;
